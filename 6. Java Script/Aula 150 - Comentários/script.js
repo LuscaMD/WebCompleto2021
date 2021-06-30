0a87481114ba25026a477d1c7d2052cf829eeb90
Lucas Martins <70406462+LuscaMD@users.noreply.github.com>
2021-06-30 15:10:18 -0300

    Aula 150 - Comentários

---
+++ 6. Java Script/Aula 150 - Comentários/script.js
@@ -0,0 +1,11 @@
+// alert('Olá, seja bem vindo ao curso');
+
+document.getElementById('nome').value = "aaaaaaaaaaaa";
+
+// Comentário de uma linha só
+
+/* Comentario de várias linhas
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+
+*/
