ade45bec35efe995ed50f9d26ff56096c7db55eb
Lucas Martins <70406462+LuscaMD@users.noreply.github.com>
2021-06-30 15:47:46 -0300

    Aula 154 - Variáveis null e undefined

---
+++ 6. Java Script/Aula 154 - Variáveis null e undefined/script.js
@@ -0,0 +1,9 @@
+// Null está apontando para o vázio
+var nome = null;
+
+// undefined está falando que não tem nada dentro
+var idade = undefined;
+// se não declarar nada, o valor padrão já é undefined
+var idade2;
+
+console.log(nome, idade, idade2);
