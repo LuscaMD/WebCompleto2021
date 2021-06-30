e56a8dd641118d49c539822e88947bf022217d6d
Lucas Martins <70406462+LuscaMD@users.noreply.github.com>
2021-06-30 15:32:41 -0300

    Aula 151, 152 - Váriaveis

---
+++ 6. Java Script/Aula 151, 152 - Variáveis/script.js
@@ -0,0 +1,15 @@
+// strings
+var texto = "Curso de javascript";
+
+// number
+var numeroInteiro = 7;
+var numeroFracionado = 7.5;
+
+// Boolean
+var teste = true; // true -> verdadeiro / false -> falso
+
+alert(numeroInteiro);
+
+document.write(texto);
+
+console.log(numeroFracionado);
