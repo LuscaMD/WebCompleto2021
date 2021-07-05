15503e190aabccf6184e5d9cbe32d5f73714f498
Lucas Martins <70406462+LuscaMD@users.noreply.github.com>
2021-07-05 16:44:16 -0300

    Aulas 168, 170, 171, 172 e 173

---
+++ 6. Java Script/Aula 168 - Switch na prática/script.js
@@ -0,0 +1,22 @@
+
+
+var escolha = prompt("Digite um numero: ");
+
+switch (escolha) {
+
+    case "1":
+        document.write("Você digitou 1");
+        break; 
+
+    case '2':
+        document.write("Você digitou 2");
+        break; 
+        
+    case '3':
+        document.write("Você digitou 3");
+        break; 
+
+    default: 
+        document.write("Default");
+        break;
+}
---
+++ 6. Java Script/Aula 172 - Precedência de execução das operações aritméticas/script.js
@@ -0,0 +1,7 @@
+
+// () => aplicar precedencia
+
+// 1° multiplicação e divisão
+// 2° adição e subtração  
+
+document.write(11-10+5*15/2);
