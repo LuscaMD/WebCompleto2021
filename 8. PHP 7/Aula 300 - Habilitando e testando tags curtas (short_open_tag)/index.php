6792e28ef4cd0837b32e7e158b4df0175a87e4de
Lucas Martins <70406462+LuscaMD@users.noreply.github.com>
2021-09-09 22:25:58 -0300

    Aula 300

---
+++ 8. PHP 7/Aula 300 - Habilitando e testando tags curtas (short_open_tag)/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Tag padrão (habilitada) -->
+    <?php
+        echo "O php parece legal";
+    ?>
+
+    <!-- Tag de impressão (habilitada) -->
+    <?= 'Essa é uma tag de impressão rápida do PHP (<?= "aqui alguma coisa" ?>), serve como um "echo"' ?> 
+
+    <!-- Tag curta (descontinuada apos o php7) -->
+    <? // codigo aqui ?>
+
+    <!-- *Asp tag (descontinuada) -->
+    <% %>
+</body>
+</html>
